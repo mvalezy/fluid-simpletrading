7fd9a86da24a129ad11d6e8d63d9f77af8e80f79
Matthieu VALEZY <mvalezy@gmail.com>
2017-10-03 00:18:58 +0200

    Fix depedencies
    Fix Table:depedencies
    Fix Chart:depedencies

--- chart.ajax.php
+++ chart.ajax.php
@@ -2,26 +2,7 @@
 
 header('Access-Control-Allow-Methods: GET');
 
-// CONFIG
-require_once('config/config.inc.php');
-
-// UTILS AND THIRD PARTY
-require_once('functions.inc.php'); 
-//require_once('config/kraken.api.config.php'); 
-//require_once('config/nma.api.config.php'); 
-
-// Class
-require_once('class/error.class.php'); 
-require_once('class/history.class.php'); 
-//require_once('class/ledger.class.php'); 
-//require_once('class/alert.class.php');
-
-// API
-//require_once('api/kraken.api.php');
-//require_once('api/nma.api.php');
-
-// Open SQL connection
-$db = connecti();
+require('depedencies.inc.php');
 
 // Query Data
 if(isset($_GET['debug']) && $_GET['debug'] > 0) { $debug = (int) $_GET['debug']; }
